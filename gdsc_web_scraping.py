5b363e7b3e768250486a7924ba290872b0232bd5
Filip Ivanovic <74300049+filip67@users.noreply.github.com>
2021-03-12 23:30:06 +0100

    Update gdsc_web_scraping.py

--- gdsc_web_scraping.py
+++ gdsc_web_scraping.py
@@ -31,13 +31,14 @@ def initialize_browser():
     # opts.add_argument('--headless')
     browser = webdriver.Firefox()
 
+
 def close_browser():
     global browser
     # browser.close()
     browser.quit()
 
 
-def pauza(_time):
+def pause(_time):
     time.sleep(_time)
 
 
@@ -48,7 +49,7 @@ def crawl_page(keyword):
     # /html/body/div[6]/div/div[8]/div[2]/div/div/div[2]/div[2]/div/div/div/div/div[2]/div/div/div[1]/div[4]/div[6]/div/div[2]/div/a/div/span/div[2]/span/span
     global browser
     all_a_links = browser.find_elements_by_xpath('//a')
-    bar_jedan = False  # ovim Bool-om proveravamo da li je strana poslednja ili ne to jest da li postoji bar jedan restoran na strani
+    at_least_one = False  # ovim Bool-om proveravamo da li je strana poslednja ili ne to jest da li postoji bar jedan restoran na strani
     total = 0
     niz = []
     for a_link in all_a_links:
@@ -57,20 +58,17 @@ def crawl_page(keyword):
             object_number_of_ratings = a_link.find_element_by_xpath('./div/span/div[1]/span[2]').text
             object_rating = a_link.find_element_by_xpath('./div/span/div[1]/span[1]').text
             object_address = a_link.find_element_by_xpath('./div/span/div[2]/span/span').text
-            # brisanje zagrada
-            object_number_of_ratings = int(object_number_of_ratings[1:-1].replace('.', ''))
-            # brisanje zagrada
+            object_number_of_ratings = int(object_number_of_ratings[1:-1].replace('.', '')) # brisanje zagrada
             print(object_name, object_rating, object_number_of_ratings, object_address)
             niz.append((object_name, object_rating, object_number_of_ratings, object_address))
-            bar_jedan = True
+            at_least_one = True
             total += 1
         except Exception as e:
             continue
     with open(keyword+".txt","w+") as file:
-        #file.write("KURCIC")
         for tuple_of_informations in niz:
             file.write('/0'.join( [str(x) for x in tuple_of_informations] )+"\n")
-    return bar_jedan, total
+    return at_least_one, total
 
 
 def crawl(keyword):
@@ -87,7 +85,7 @@ def crawl(keyword):
     google_search(browser, city + " " + keyword)
     all_a_links = browser.find_elements_by_xpath('//a')
     right_a_link = None
-    pauza(5.0)
+    pause(5.0)
     right_a_link = browser.find_element_by_xpath('//a[@class="Q2MMlc"]')
     """
     for a_object in all_a_links:
@@ -98,15 +96,15 @@ def crawl(keyword):
     right_a_link.click()
     total = 0
     while (True):
-        pauza(5.0)
-        bar_jedan, new_total = crawl_page(keyword)
+        pause(5.0)
+        at_least_one, new_total = crawl_page(keyword)
         total += new_total
         if total >= 10:
             break
-        if not bar_jedan:
+        if not at_least_one:
             print("ALL", keyword, "FOUND")
             break
-        pauza(2.0)
+        pause(2.0)
         try:
             next_page = browser.find_element_by_xpath('//a[@id="pnnext"]')
             next_page.click()
@@ -114,26 +112,27 @@ def crawl(keyword):
             print("ALL", keyword, "FOUND")
             break
 
+
 def crawl_hotels():
-    #/html/body/div[7]/div/div[9]/div[1]/div/div[2]/div[2]/div/div/div[1]/div/div/div/div/div[3]/div/g-more-link/a
+    # /html/body/div[7]/div/div[9]/div[1]/div/div[2]/div[2]/div/div/div[1]/div/div/div/div/div[3]/div/g-more-link/a
     global city
     global browser
     search = city + " hotels"
     google_search(browser,search)
-    pauza(12.0)
+    pause(12.0)
     view_more_button = browser.find_element_by_xpath('/html/body/div[7]/div/div[9]/div[1]/div/div[2]/div[2]/div/div/div[1]/div/div/div/div/div[3]/div/g-more-link/a')
     view_more_button.click()
-    #/html/body/c-wiz[2]/div/div[2]/div/c-wiz/div/div[1]/div[2]/main/div/div[2]/c-wiz/div[6]/c-wiz[1]/div/a
-    #/html/body/c-wiz[2]/div/div[2]/div/c-wiz/div/div[1]/div[2]/main/div/div[2]/c-wiz/div[6]/c-wiz[2]/div/a
-    #/html/body/c-wiz[2]/div/div[2]/div/c-wiz/div/div[1]/div[2]/main/div/div[2]/c-wiz/div[6]/c-wiz[16]/div/a
+    # /html/body/c-wiz[2]/div/div[2]/div/c-wiz/div/div[1]/div[2]/main/div/div[2]/c-wiz/div[6]/c-wiz[1]/div/a
+    # /html/body/c-wiz[2]/div/div[2]/div/c-wiz/div/div[1]/div[2]/main/div/div[2]/c-wiz/div[6]/c-wiz[2]/div/a
+    # /html/body/c-wiz[2]/div/div[2]/div/c-wiz/div/div[1]/div[2]/main/div/div[2]/c-wiz/div[6]/c-wiz[16]/div/a
     all_hotels = browser.find_elements_by_xpath('//c-wiz')
-    pauza(10.0)
+    pause(10.0)
     for hotel in all_hotels:
         #print("HERE")
         try:
-            #/html/body/c-wiz[2]/div/div[2]/div/c-wiz/div/div[1]/div[2]/main/div/div[2]/c-wiz/div[6]/c-wiz[1]/div/div/div/div[1]/div/div[1]/div[2]/a/div/div/div/span[1]/span/span[1]
-            hotel_root_element = hotel.find_element_by_xpath('./div/a')#ovo je <a> koji sadrzi sve podatke o konkretnom hotelu
-            hotel_name = hotel_root_element.get_attribute("aria-label")#text
+            # /html/body/c-wiz[2]/div/div[2]/div/c-wiz/div/div[1]/div[2]/main/div/div[2]/c-wiz/div[6]/c-wiz[1]/div/div/div/div[1]/div/div[1]/div[2]/a/div/div/div/span[1]/span/span[1]
+            hotel_root_element = hotel.find_element_by_xpath('./div/a') # ovo je <a> koji sadrzi sve podatke o konkretnom hotelu
+            hotel_name = hotel_root_element.get_attribute("aria-label") # text
             hotel_price = hotel.find_element_by_xpath('./div/div/div/div[1]/div/div[1]/div[2]/a/div/div/div/span[1]/span/span[1]').text
             hotel_price = hotel_price.replace(',','')
             hotel_price = hotel_price.replace('.', '')
@@ -144,6 +143,7 @@ def crawl_hotels():
             continue
     browser.quit()
 
+
 def get_hotel_location(hotel_name):
     # /html/body/div[7]/div/div[9]/div[3]/div/div[1]/div/div[1]/div/div[4]/div/div[2]/div/div/span[2]
     # /html/body/div[7]/div/div[9]/div[3]/div/div[1]/div/div[1]/div/div[4]/div/div[2]/div/div/span[2]
@@ -153,15 +153,15 @@ def get_hotel_location(hotel_name):
         hotel_name = hotel_name + " " + city
     temp_browser = webdriver.Firefox()
     google_search(temp_browser, hotel_name)
-    pauza(13.0)
+    pause(13.0)
     # /html/body/div[7]/div/div[9]/div[2]/div/div[1]/div/div[1]/div/div[4]/div/div[2]/div/div/span[2]
     try:
         customer_location = temp_browser.find_element_by_xpath(
             '/html/body/div[7]/div/div[9]/div[2]/div/div[1]/div/div[1]/div/div[4]/div/div[2]/div/div/span[2]').text
         print(customer_location)
     except:
-        return -1;
-    return 1;
+        return -1
+    return 1
     temp_browser.quit()
 
 
@@ -175,13 +175,13 @@ def main():
         print("ERROR WHILE BROWSER INITIALIZATION")
         close_browser()
         exit()
-    city = input("Please enter city name:")
-    customer_hotel = input("Please enter name of your hotel(alternatively enter -1 to input address of your location):")
+    city = input("Please enter city name: ")
+    customer_hotel = input("Please enter name of your hotel(alternatively enter -1 to input address of your location): ")
     hotel_location_success = None
     if customer_hotel != "-1":
         hotel_location_success = get_hotel_location(customer_hotel)
     if customer_hotel == "-1" or hotel_location_success == -1:
-        customer_location = input("Please enter your location:")
+        customer_location = input("Please enter your location: ")
     if city == None:
         print("ERROR WHILE CITY NAME INPUT")
         close_browser()
@@ -196,5 +196,6 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
 # python3 /Users/mbp/Downloads/gdsc_web_scraping.py
